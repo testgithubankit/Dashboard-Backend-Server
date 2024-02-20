1ed8ea4dffab80b9800be91295b1747b6497de25
Saurabh Kumar Tiwari <sky.royaltiwari@gmail.com>
2024-02-20 12:56:54 +0530

    added

--- src/main/java/com/paymentDashboard/dashboard/DashboardApplication.java
+++ src/main/java/com/paymentDashboard/dashboard/DashboardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DashboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DashboardApplication.class, args);
+		 SpringApplication.run(DashboardApplication.class, args);
 	}
 
 }
